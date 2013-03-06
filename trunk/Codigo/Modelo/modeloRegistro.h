1627db142f9721df4a5a92504494bf9d7de1c4f1
poniendowebs@gmail.com <poniendowebs@gmail.com@ac7e1297-91f6-f731-f1be-fe0b952bfef4>
2013-03-06 15:34:20 +0000

    Funcion lecturaFichero y procesarRegistro

---
+++ trunk/Codigo/Modelo/modeloRegistro.h
@@ -0,0 +1,84 @@
+// -*- C++ -*-
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef _MODELOREGISTRO_H_
+#define _MODELOREGISTRO_H_
+
+#include <string>
+#include "modelo.h"
+
+using namespace std;
+
+/*
+ * Clase:     ModeloRegistro
+ * Fecha:     Tue Sep 12 16:36:32 CEST 2000
+ * Autor:     Antonio-M. Corbi Bellot
+ * Email:     acorbi@dlsi.ua.es
+ * Proposito: Representa el modelo de una persona.
+ */
+
+/*!
+ * \class ModeloRegistro
+ * \brief Es el modelo de una persona en este ejemplo.
+ */
+class ModeloRegistro : public Modelo
+{
+  // Parte publica
+public:
+
+  /*!
+   * \brief Constructor por defecto. Necesita nombre y edad.
+   * \param n El nombre de la persona.
+   * \param e La edad de la persona.
+   */
+  ModeloRegistro(const string &n, int e );
+  //! Constructor de copia
+  ModeloRegistro(const ModeloRegistro &un_modeloRegistro);
+
+  //! Destructor
+  ~ModeloRegistro();
+
+  //! Asignacion
+  ModeloRegistro & operator =(const ModeloRegistro &un_modeloRegistro);
+
+  /*
+   * Métodos Leer/Poner
+   */
+
+  //! Devuelve el nombre de una persona.
+  string getNombre() { return nombre; }
+  //! Pone el nombre a una persona.
+  void setNombre(const string &nombre) { nombre = nombre; }
+
+
+  // Parte protegida
+protected:
+  //! Nombre de la persona.
+  string nombre;
+	string direccion;
+	string poblacion;
+	string cpostal;
+	string telefono;
+	string email;
+
+  // Parte privada
+private:
+
+};
+
+#endif
