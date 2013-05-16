6bcdd508e7518a6067f356487548f01b91df8587
pablovm1990@gmail.com <pablovm1990@gmail.com@ac7e1297-91f6-f731-f1be-fe0b952bfef4>
2013-05-16 12:28:21 +0000

    Mostrando datos

--- trunk/Codigo/Vista-qt/main.cpp
+++ trunk/Codigo/Vista-qt/main.cpp
@@ -19,6 +19,11 @@ int main(int argc, char *argv[])
 	{
         ar=mr.GetRegistro(0);
         ui.INombre->setPlainText(QString::fromUtf8( ar[0].c_str() ));
+        ui.IDireccion->setPlainText(QString::fromUtf8( ar[1].c_str() ));
+        ui.IPoblacion->setPlainText(QString::fromUtf8( ar[2].c_str() ));
+        ui.ICPostal->setPlainText(QString::fromUtf8( ar[3].c_str() ));
+        ui.ITelefono->setPlainText(QString::fromUtf8( ar[4].c_str() ));
+        ui.IMail->setPlainText(QString::fromUtf8( ar[5].c_str() ));
 	}
      return app.exec();
  }
