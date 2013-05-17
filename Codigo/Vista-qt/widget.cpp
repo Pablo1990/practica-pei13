328d0d1e711693cfc0107ceabe60ac4a68cd1c2e
pablovm1990@gmail.com <pablovm1990@gmail.com@ac7e1297-91f6-f731-f1be-fe0b952bfef4>
2013-05-17 10:50:00 +0000

    Movimiento entre registros del fichero OK

--- Codigo/Vista-qt/widget.cpp
+++ Codigo/Vista-qt/widget.cpp
@@ -16,13 +16,24 @@ Widget::~Widget()
 
 void Widget::on_BtNextRegistro_clicked()
 {
-
+    std::vector<std::string> ar=mr.GetRegistro(ui->LbId->text().toInt()+1);
+    if(ar.size()==6)
+    {
+        ui->INombre->setPlainText(QString::fromUtf8( ar[0].c_str() ));
+        ui->IDireccion->setPlainText(QString::fromUtf8( ar[1].c_str() ));
+        ui->IPoblacion->setPlainText(QString::fromUtf8( ar[2].c_str() ));
+        ui->ICPostal->setPlainText(QString::fromUtf8( ar[3].c_str() ));
+        ui->ITelefono->setPlainText(QString::fromUtf8( ar[4].c_str() ));
+        ui->IMail->setPlainText(QString::fromUtf8( ar[5].c_str() ));
+        QString str;
+        str.append(QString("%1").arg((ui->LbId->text().toInt()+1)));
+        ui->LbId->setText(str);
+    }
 }
 
 void Widget::on_pushButton_clicked()
 {
     QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), "", tr("Files (*.*)"));
-    ModeloRegistro mr;
     std::vector<std::string> ar;
     if(mr.LeerFichero(fileName.toAscii().data()))
     {
@@ -34,5 +45,22 @@ void Widget::on_pushButton_clicked()
         ui->ITelefono->setPlainText(QString::fromUtf8( ar[4].c_str() ));
         ui->IMail->setPlainText(QString::fromUtf8( ar[5].c_str() ));
         ui->LbId->setText("0");
+    }
+}
+
+void Widget::on_BtPrevRegistro_clicked()
+{
+    std::vector<std::string> ar=mr.GetRegistro(ui->LbId->text().toInt()-1);
+    if(ar.size()==6)
+    {
+        ui->INombre->setPlainText(QString::fromUtf8( ar[0].c_str() ));
+        ui->IDireccion->setPlainText(QString::fromUtf8( ar[1].c_str() ));
+        ui->IPoblacion->setPlainText(QString::fromUtf8( ar[2].c_str() ));
+        ui->ICPostal->setPlainText(QString::fromUtf8( ar[3].c_str() ));
+        ui->ITelefono->setPlainText(QString::fromUtf8( ar[4].c_str() ));
+        ui->IMail->setPlainText(QString::fromUtf8( ar[5].c_str() ));
+        QString str;
+        str.append(QString("%1").arg((ui->LbId->text().toInt()-1)));
+        ui->LbId->setText(str);
     }
 }
