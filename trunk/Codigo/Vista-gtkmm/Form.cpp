de8e79089910e406d9994115dbf19a59d29a104e
pablovm1990@gmail.com <pablovm1990@gmail.com@ac7e1297-91f6-f731-f1be-fe0b952bfef4>
2013-06-04 17:53:07 +0000

    Anyadiendo las mierdas iniciales

---
+++ trunk/Codigo/Vista-gtkmm/Form.cpp
@@ -0,0 +1,72 @@
+#include "Form.h"
+
+using namespace Gtk;
+
+FrmMain::FrmMain(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& refGlade) :
+    Gtk::Window(cobject), builder(refGlade){
+
+    builder->get_widget("BtNuevoRegistro", BtNuevoRegistro);
+    builder->get_widget("BtGuardarRegistro", BtGuardarRegistro);
+    builder->get_widget("BtModifcar", BtModifcar);
+    builder->get_widget("BtBorrarRegistro", BtBorrarRegistro);
+    builder->get_widget("BtPrevRegistro", BtPrevRegistro);
+    builder->get_widget("BtNextRegistro", BtNextRegistro);
+    builder->get_widget("BtGuardarFichero", BtGuardarFichero);
+    builder->get_widget("BtBuscar", BtBuscar);
+    builder->get_widget("ChMostrar", ChMostrar);
+    builder->get_widget("EnNombre", EnNombre);
+    builder->get_widget("EnDireccion", EnDireccion);
+    builder->get_widget("EnPoblacion", EnPoblacion);
+    builder->get_widget("EnCPostal", EnCPostal);
+    builder->get_widget("EnTelefono", EnTelefono);
+    builder->get_widget("EnMail", EnMail);
+    builder->get_widget("EnBusqueda", EnBusqueda);
+
+    BtNuevoRegistro->signal_clicked().connect(sigc::mem_fun(*this, &Form::on_BtNuevoRegistro_clicked));
+    BtGuardarRegistro->signal_clicked().connect(sigc::mem_fun(*this, &Form::on_BtGuardarRegistro_clicked));
+    BtGuardarRegistro->signal_clicked().connect(sigc::mem_fun(*this, &Form::on_BtGuardarRegistro_clicked));
+    BtGuardarRegistro->signal_clicked().connect(sigc::mem_fun(*this, &Form::on_BtGuardarRegistro_clicked));
+    BtGuardarRegistro->signal_clicked().connect(sigc::mem_fun(*this, &Form::on_BtGuardarRegistro_clicked));
+    BtGuardarRegistro->signal_clicked().connect(sigc::mem_fun(*this, &Form::on_BtGuardarRegistro_clicked));
+    BtGuardarRegistro->signal_clicked().connect(sigc::mem_fun(*this, &Form::on_BtGuardarRegistro_clicked));
+    BtGuardarRegistro->signal_clicked().connect(sigc::mem_fun(*this, &Form::on_BtGuardarRegistro_clicked));
+    BtGuardarRegistro->signal_clicked().connect(sigc::mem_fun(*this, &Form::on_BtGuardarRegistro_clicked));
+
+}
+
+void Form::on_BtNuevoRegistro_clicked(){
+
+}
+
+void Form::on_BtGuardarRegistro_clicked(){
+
+}
+
+void Form::on_BtModificar_clicked(){
+
+}
+
+void Form::on_BtBorrarRegistro_clicked(){
+
+}
+
+void Form::on_BtPrevRegistro_clicked(){
+
+}
+
+void Form::on_BtNextRegistro_clicked(){
+
+}
+
+void Form::on_BtGuardarFichero_clicked(){
+
+}
+
+void Form::on_ChMostrar_clicked(){
+
+}
+
+void Form::on_BtBuscar_clicked(){
+
+}
+
