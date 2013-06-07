579276089f8ce17eb115d0339bd55e2797202696
pablovm1990@gmail.com <pablovm1990@gmail.com@ac7e1297-91f6-f731-f1be-fe0b952bfef4>
2013-06-07 10:22:27 +0000

    Limpiando

--- trunk/Codigo/Modelo/modeloRegistro.h
+++ trunk/Codigo/Modelo/modeloRegistro.h
@@ -1,45 +1,21 @@
-// -*- C++ -*-
-
-/*
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Library General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- */
-
 #ifndef _MODELOREGISTRO_H_
 #define _MODELOREGISTRO_H_
 
 #include <string>
 #include <vector>
-#include "modelo.h"
+#include <cstdlib>
+#include <list>
+#include <stddef.h>
 #include "Registro.h"
 
 class Registro;
 using namespace std;
 
-/*
- * Clase:     ModeloRegistro
- * Fecha:     Tue Sep 12 16:36:32 CEST 2000
- * Autor:     Antonio-M. Corbi Bellot
- * Email:     acorbi@dlsi.ua.es
- * Proposito: Representa el modelo de una persona.
- */
-
 /*!
  * \class ModeloRegistro
  * \brief Es el modelo de una persona en este ejemplo.
  */
-class ModeloRegistro : public Modelo
+class ModeloRegistro
 {
   friend class Registro;
   // Parte publica
